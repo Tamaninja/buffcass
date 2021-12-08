c25f576d049bd6dc916de32bc060846d74176386
Metro Panties <71499708+MetroPanties@users.noreply.github.com>
2021-12-08 13:53:31 +0000

    commit

---
+++ src/test/java/me/tamaninja/buffcass/buffcass/BuffcassApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tamaninja.buffcass.buffcass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuffcassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
