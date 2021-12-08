ab1702801751cfcfc538746ddd064e03de9fdbeb
Metro Panties <71499708+MetroPanties@users.noreply.github.com>
2021-12-08 13:54:57 +0000

    typo fix

--- src/main/java/me/tamaninja/buffcass/controllers/TestController.java
+++ src/main/java/me/tamaninja/buffcass/controllers/TestController.java
@@ -1,4 +1,4 @@
-package me.tamaninja.buffcass.buffcass.controllers;
+package me.tamaninja.buffcass.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/test/java/me/tamaninja/buffcass/BuffcassApplicationTests.java
+++ src/test/java/me/tamaninja/buffcass/BuffcassApplicationTests.java
@@ -1,4 +1,4 @@
-package me.tamaninja.buffcass.buffcass;
+package me.tamaninja.buffcass;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
